75d2dbc3ae69adeca0dc7e5f9c9397d431945f0a
s.sagawa <s.sagawa@sunday-ja.com>
2021-03-23 16:30:34 +0900

    add methodtuika

--- app/Utils/StrtotimeConverter.php
+++ app/Utils/StrtotimeConverter.php
@@ -15,6 +15,9 @@ class StrtotimeConverter
     private const COLONDLIMITER = '/:/';
     private const COLON = ':';
 
+    private const TIME_FORMAT_HI = 'H:i';
+    private const TIME_FORMAT_HIS = 'H:i:s';
+
     public static function strHourToIntMinute(String $hour)
     {
         if (!preg_match(self::HOURMINUTESPATTERN, $hour)) return false;
@@ -30,5 +33,16 @@ class StrtotimeConverter
         $modMinutes = $minutes % self::MINUTESFORHOUR;
 
         return str_pad($intHour, 2, '0', STR_PAD_LEFT) . self::COLON . str_pad($modMinutes, 2, '0', STR_PAD_LEFT);
+    }
+
+    public static function convertTimeFormat(String $time)
+    {
+        if (preg_match('/^([0-1][0-9]|[2][0-4]):[0-5][0-9]:[0-9]{2,3}$/', $time)) {
+            return preg_replace('/:[0-9]{2}$/', '', $time);
+        } else if (preg_match('/^([0-1][0-9]|[2][0-4]):[0-5][0-9]/', $time)) {
+            return $time . ':00';
+        }
+
+        return false;
     }
 }
